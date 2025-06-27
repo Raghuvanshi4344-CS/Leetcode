b1b55e3d1a72aba91cbeda8017b23ed712be7775
Raghuvanshi4344-CS <priyansh0975@gmail.com>
2025-06-27 19:33:42 +0530

    Time: 2553 ms (5%), Space: 55.8 MB (72.27%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.java
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -10,13 +10,23 @@ class Solution {
         //     arr[min]=temp;
         // }
         
+        // for(int i=0;i<arr.length;i++){
+        //     for(int j=0;j<arr.length;j++){
+        //         if(arr[i]<arr[j]){
+        //         int temp=arr[i];
+        //         arr[i]=arr[j];
+        //         arr[j]=temp;}
+        //     }
+        // }
+
+
         for(int i=0;i<arr.length;i++){
-            for(int j=0;j<arr.length;j++){
-                if(arr[i]<arr[j]){
-                int temp=arr[i];
-                arr[i]=arr[j];
-                arr[j]=temp;}
+            int k=arr[i];
+            int j;
+            for(j=i-1;j>=0 && arr[j]>k;j--){
+                    arr[j+1]=arr[j];
             }
+            arr[j+1]=k;
         }
         return arr;
     }
