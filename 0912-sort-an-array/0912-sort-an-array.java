950edff8be86ae87e6466b40499ebf94929032f2
Raghuvanshi4344-CS <priyansh0975@gmail.com>
2025-06-27 19:33:16 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] sortArray(int[] arr) {
+        // for(int i=0;i<arr.length;i++){
+        //     int min=i;
+        //     for(int j=i+1;j<arr.length;j++){
+        //         if(arr[j]<arr[min]){min=j;}
+        //     }
+        //     int temp=arr[i];
+        //     arr[i]=arr[min];
+        //     arr[min]=temp;
+        // }
+        
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr.length;j++){
+                if(arr[i]<arr[j]){
+                int temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;}
+            }
+        }
+        return arr;
+    }
+}
