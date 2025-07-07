31caf7f152bba79c0ac24ae4b00fac1d05169f07
Raghuvanshi4344-CS <priyansh0975@gmail.com>
2025-07-07 12:41:04 +0530

    Time: 0 ms (100%), Space: 41.2 MB (53.22%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -14,20 +14,10 @@
  * }
  */
 class Solution {
-    void func(TreeNode root,List<Integer> temp){
-        if(root==null){
-            temp.add(null);
-            return;
-        }
-        temp.add(root.val);
-        func(root.left,temp);
-        func(root.right,temp);
-    }
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        List<Integer> temp1=new ArrayList<>();
-        func(p,temp1);
-        List<Integer> temp2=new ArrayList<>();
-        func(q,temp2);
-        return temp1.equals(temp2);
+        if(p==null && q==null) return true;
+        if(p==null || q==null) return false;
+        if(p.val!=q.val) return false;
+        return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
     }
 }
