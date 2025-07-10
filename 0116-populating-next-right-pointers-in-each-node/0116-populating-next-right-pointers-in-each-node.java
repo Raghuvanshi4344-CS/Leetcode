7d1a93e6b0d480fe45e68dfc16011122e89dea67
Raghuvanshi4344-CS <priyansh0975@gmail.com>
2025-07-10 19:56:59 +0530

    Time: 0 ms (100%), Space: 45.1 MB (5.26%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.java
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.java
@@ -22,27 +22,42 @@ class Node {
 */
 
 class Solution {
-    void func(Node root){
-        if(root==null) return;
-        Queue<Node> q=new LinkedList<>();
-        q.add(root);
-        while(!q.isEmpty()){
-            int size = q.size();
-            Node prev = null;
+    // void func(Node root){
+    //     if(root==null) return;
+    //     Queue<Node> q=new LinkedList<>();
+    //     q.add(root);
+    //     while(!q.isEmpty()){
+    //         int size = q.size();
+    //         Node prev = null;
 
-            for (int i = 0; i < size; i++) {
-                Node curr = q.poll();
-                if (prev != null) {
-                    prev.next = curr;
-                }
-                prev = curr;
-                if (curr.left != null) q.add(curr.left);
-                if (curr.right != null) q.add(curr.right);
-            }
-        }
-    }
+    //         for (int i = 0; i < size; i++) {
+    //             Node curr = q.poll();
+    //             if (prev != null) {
+    //                 prev.next = curr;
+    //             }
+    //             prev = curr;
+    //             if (curr.left != null) q.add(curr.left);
+    //             if (curr.right != null) q.add(curr.right);
+    //         }
+    //     }
+    // }
     public Node connect(Node root) {
-        func(root);
+        // func(root);
+        // return root;
+        if (root == null || root.left == null || root.right == null) {
+            return root;
+        }
+
+        root.left.next = root.right;
+
+        if (root.next != null) {
+            root.right.next = root.next.left;
+        }
+
+        connect(root.left);
+        connect(root.right);
+
         return root;
     }
+    
 }
