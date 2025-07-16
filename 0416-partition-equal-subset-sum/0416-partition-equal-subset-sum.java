0bb7490892daa7d93065205e51c7201c151615d9
Priyanshu Raghuvanshi <priyansh0975@gmail.com>
2025-07-16 22:24:26 +0530

    Time: 27 ms (84.44%), Space: 57.7 MB (19.2%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -6,7 +6,7 @@ class Solution {
             s+=nums[i];
         }
         if(s%2!=0) return false;
-        s=s/2;
+        s/=2;
         int arr[][]=new int [n+1][s+1];
         arr[0][0]=1;
         for(int i=1;i<=n;i++){
@@ -20,7 +20,6 @@ class Solution {
             }
         }
         if(arr[n][s]!=0) return true;
-        System.out.println(arr[n][s]);
         return false;
     }
 }
