852c7eca3d1e5a14a9d1a46e65b0b1aaa71c5922
Priyanshu Raghuvanshi <priyansh0975@gmail.com>
2025-07-17 23:44:21 +0530

    Time: 0 ms (100%), Space: 45.8 MB (59.27%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -3,7 +3,7 @@ class Solution {
         int max=0;
         for(int i=0;i<arr.length-1;i++){
             if(arr[i]<arr[i+1]){
-                max=max+arr[i+1]-arr[i];
+                max+=arr[i+1]-arr[i];
             }
         }
         return max;
