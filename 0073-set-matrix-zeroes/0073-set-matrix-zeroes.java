215f48f2d7b0301979eb34e389bf142accab07ca
Priyanshu Raghuvanshi <priyansh0975@gmail.com>
2025-07-28 22:59:01 +0530

    Time: 1 ms (72.29%), Space: 45.7 MB (45.91%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,16 +1,35 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        List<Pair<Integer, Integer>> in = new ArrayList<>();
+        // List<Pair<Integer, Integer>> in = new ArrayList<>();
+        // for(int i=0;i<matrix.length;i++){
+        //     for(int j=0;j<matrix[0].length;j++){
+        //         if(matrix[i][j]==0 ){
+        //             in.add(new Pair<>(i,j));
+        //         }
+        //     }
+        // }
+        // for(int p=0;p<in.size();p++){
+        //     int row=in.get(p).getKey();
+        //     int col=in.get(p).getValue();
+        //     for(int i=0;i<matrix.length;i++){
+        //         matrix[i][col]=0;
+        //     }
+        //     for(int i=0;i<matrix[0].length;i++){
+        //         matrix[row][i]=0;
+        //     }
+        // }
+        
+        List<Pair<Integer,Integer>> re=new ArrayList<>();
         for(int i=0;i<matrix.length;i++){
             for(int j=0;j<matrix[0].length;j++){
-                if(matrix[i][j]==0 ){
-                    in.add(new Pair<>(i,j));
+                if(matrix[i][j]==0){
+                    re.add(new Pair<>(i,j));
                 }
             }
         }
-        for(int p=0;p<in.size();p++){
-            int row=in.get(p).getKey();
-            int col=in.get(p).getValue();
+        for(int p=0;p<re.size();p++){
+            int row=re.get(p).getKey();
+            int col=re.get(p).getValue();
             for(int i=0;i<matrix.length;i++){
                 matrix[i][col]=0;
             }
