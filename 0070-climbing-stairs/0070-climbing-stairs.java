9f5340908c694d28bfa599e3a76c47a84421d086
Priyanshu Raghuvanshi <priyansh0975@gmail.com>
2026-02-24 22:24:05 +0530

    Time: 0 ms (100%), Space: 42.1 MB (54.6%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,6 +1,6 @@
 class Solution {
     public int climbStairs(int n) {
-        int arr[]=new int[n+1];
+        int arr[] =new int [n+1];
         if(n<2) return 1;
         arr[0]=1;
         arr[1]=1;
