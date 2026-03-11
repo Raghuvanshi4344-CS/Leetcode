adbd6a4cfda780c52232aa49f097baa49a629beb
Priyanshu Raghuvanshi <priyansh0975@gmail.com>
2026-03-11 18:54:54 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0203-remove-linked-list-elements/0203-remove-linked-list-elements.java
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.java
@@ -8,48 +8,48 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-// class Solution {
-//     public ListNode removeElements(ListNode head, int val) {
-//         if(head==null) return head;
-//         while(head != null && head.val == val) {
-//             head = head.next;
-//         }
-//         ListNode temp=head;
-//         while(temp!=null && temp.next!=null){
-//             if(temp.next.val==val){
-//                 temp.next=temp.next.next;
-//             }
-//             else {
-//                 temp=temp.next;
-//             }
-//         }
-//         return head;
-//     }
-// }
-
 class Solution {
-    ListNode fin;
-    public void addf(int data){
-        ListNode newNode=new ListNode(data);
-        if(fin==null) {
-            fin=newNode;
-            return;
-        }
-        ListNode temp=fin;
-        while(temp.next!=null){
-            temp=temp.next;
-        }
-        temp.next=newNode;
-    }
     public ListNode removeElements(ListNode head, int val) {
-        if (head==null) return head;
-        fin=null;
-        while(head!=null){
-            if(head.val!=val){
-                addf(head.val);
-            }
-            head=head.next;
+        if(head==null) return head;
+        while(head != null && head.val == val) {
+            head = head.next;
         }
-        return fin;
+        ListNode temp=head;
+        while(temp!=null && temp.next!=null){
+            if(temp.next.val==val){
+                temp.next=temp.next.next;
+            }
+            else {
+                temp=temp.next;
+            }
+        }
+        return head;
     }
 }
+
+// class Solution {
+//     ListNode fin;
+//     public void addf(int data){
+//         ListNode newNode=new ListNode(data);
+//         if(fin==null) {
+//             fin=newNode;
+//             return;
+//         }
+//         ListNode temp=fin;
+//         while(temp.next!=null){
+//             temp=temp.next;
+//         }
+//         temp.next=newNode;
+//     }
+//     public ListNode removeElements(ListNode head, int val) {
+//         if (head==null) return head;
+//         fin=null;
+//         while(head!=null){
+//             if(head.val!=val){
+//                 addf(head.val);
+//             }
+//             head=head.next;
+//         }
+//         return fin;
+//     }
+// }
