d2e546c0df0e10a55e1e643b2afe7df54f3d3865
Priyanshu Raghuvanshi <priyansh0975@gmail.com>
2026-03-11 18:46:06 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0203-remove-linked-list-elements/0203-remove-linked-list-elements.java
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.java
@@ -8,21 +8,44 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+// class Solution {
+//     public ListNode removeElements(ListNode head, int val) {
+//         if(head==null) return head;
+//         while(head != null && head.val == val) {
+//             head = head.next;
+//         }
+//         ListNode temp=head;
+//         while(temp!=null && temp.next!=null){
+//             if(temp.next.val==val){
+//                 temp.next=temp.next.next;
+//             }
+//             else {
+//                 temp=temp.next;
+//             }
+//         }
+//         return head;
+//     }
+// }
+
 class Solution {
+    ListNode new fin;
+    public void add(int data){
+        ListNode newNode=new ListNode(data);
+        if(head==null){
+            head=newNode;
+            return;
+        }
+        newNode.next=head;
+        head=newNode;
+    }
     public ListNode removeElements(ListNode head, int val) {
-        if(head==null) return head;
-        while(head != null && head.val == val) {
-            head = head.next;
-        }
-        ListNode temp=head;
-        while(temp!=null && temp.next!=null){
-            if(temp.next.val==val){
-                temp.next=temp.next.next;
+        if (head==null) return head;
+        ListNode new fin;
+        while(head!=null){
+            if(head.val!=val){
+                fin.add(val)
             }
-            else {
-                temp=temp.next;
-            }
+            head=head.next;
         }
-        return head;
     }
 }
