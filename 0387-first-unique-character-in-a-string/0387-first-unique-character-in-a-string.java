1c8dd6aac70ff4cc0432b44a26c2b339f78fdb80
Priyanshu Raghuvanshi <priyansh0975@gmail.com>
2026-03-12 18:53:13 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -1,19 +1,36 @@
+// class Solution {
+//     public int firstUniqChar(String s) {
+//         Queue<Character> q=new LinkedList<>();
+//         Set<Character> removed=new HashSet<>();
+//         for(int i=0;i<s.length();i++){
+//             // q.add(String.valueOf(s.charAt(i))); ////if we use string in queue
+//             q.add(s.charAt(i));
+//         }
+//         int y=0;
+//         while(!q.isEmpty()){
+//             char x=q.poll();
+//             if (!q.contains(x)&&!removed.contains(x)) {
+//                 return y;
+//             }
+//             removed.add(x);
+//             y++;
+//         }
+//         return -1;
+//     }
+// }
+
+
+
 class Solution {
     public int firstUniqChar(String s) {
-        Queue<Character> q=new LinkedList<>();
-        Set<Character> removed=new HashSet<>();
-        for(int i=0;i<s.length();i++){
-            // q.add(String.valueOf(s.charAt(i))); ////if we use string in queue
-            q.add(s.charAt(i));
+        int[] freq = new int[26];
+        for(int i = 0; i < s.length(); i++){
+            freq[s.charAt(i) - 'a']++;
         }
-        int y=0;
-        while(!q.isEmpty()){
-            char x=q.poll();
-            if (!q.contains(x)&&!removed.contains(x)) {
-                return y;
+        for(int i = 0; i < s.length(); i++){
+            if(freq[s.charAt(i) - 'a'] == 1){
+                return i;
             }
-            removed.add(x);
-            y++;
         }
         return -1;
     }
