21fd0ad3788dccbe617a266db2e3f0aedcfae961
Priyanshu Raghuvanshi <priyansh0975@gmail.com>
2026-03-12 18:51:26 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -1,17 +1,18 @@
 class Solution {
     public int firstUniqChar(String s) {
         Queue<Character> q=new LinkedList<>();
+        Set<Character> removed=new HashSet<>();
         for(int i=0;i<s.length();i++){
             // q.add(String.valueOf(s.charAt(i))); ////if we use string in queue
             q.add(s.charAt(i));
         }
         int y=0;
         while(!q.isEmpty()){
-            char x=q.peek();
-            q.remove();
-            if (s.indexOf(x) == s.lastIndexOf(x)) {
+            char x=q.poll();
+            if (!q.contains(x)&&!removed.contains(x)) {
                 return y;
             }
+            removed.add(x);
             y++;
         }
         return -1;
