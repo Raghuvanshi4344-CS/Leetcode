081e3b04d5091e6db06febd97577f6fe50f0c576
Priyanshu Raghuvanshi <priyansh0975@gmail.com>
2026-03-14 17:46:57 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.java
+++ 0071-simplify-path/0071-simplify-path.java
@@ -1,25 +1,76 @@
+// class Solution {
+//     public String simplifyPath(String path) {
+//         Stack<String> s=new Stack<>();
+//         String []part=path.split("/");
+//         for(int i=0;i<part.length;i++){
+//             if(part[i].equals("") || part[i].equals(".")){
+//                 continue;
+//             }
+//             if(part[i].equals("..")){
+//                 if(!s.isEmpty()){
+//                     s.pop();
+//                 }
+//             }
+//             else{
+//                 s.push(part[i]);
+//             }
+//         }
+//         if(s.isEmpty()) return "/";
+//         String y="";
+//         for(int i=0;i<s.size();i++){
+//             y=y+"/"+s.get(i);
+//         }
+//         return y;
+//     }
+// }
+
+
+
+
+
+
+
+import java.util.*;
+
 class Solution {
     public String simplifyPath(String path) {
-        Stack<String> s=new Stack<>();
-        String []part=path.split("/");
-        for(int i=0;i<part.length;i++){
-            if(part[i].equals("") || part[i].equals(".")){
-                continue;
-            }
-            if(part[i].equals("..")){
-                if(!s.isEmpty()){
-                    s.pop();
+        Stack<String> s = new Stack<>();
+        String dir = "";  // current directory name
+
+        for (int i = 0; i < path.length(); i++) {
+            char c = path.charAt(i);
+
+            if (c == '/') {
+                if (!dir.equals("")) {        // end of a directory
+                    if (dir.equals("..")) {  // go up
+                        if (!s.isEmpty()) s.pop();
+                    } else if (!dir.equals(".")) { // ignore "."
+                        s.push(dir);
+                    }
+                    dir = "";  // reset for next directory
                 }
-            }
-            else{
-                s.push(part[i]);
+            } else {
+                dir += c;  // build the directory name
             }
         }
-        if(s.isEmpty()) return "/";
-        String y="";
-        for(int i=0;i<s.size();i++){
-            y=y+"/"+s.get(i);
+
+        // Handle last directory if path doesn't end with '/'
+        if (!dir.equals("")) {
+            if (dir.equals("..")) {
+                if (!s.isEmpty()) s.pop();
+            } else if (!dir.equals(".")) {
+                s.push(dir);
+            }
         }
-        return y;
+
+        // Build the final path
+        if (s.isEmpty()) return "/";
+
+        StringBuilder y = new StringBuilder();
+        for (String d : s) {
+            y.append("/").append(d);
+        }
+
+        return y.toString();
     }
 }
