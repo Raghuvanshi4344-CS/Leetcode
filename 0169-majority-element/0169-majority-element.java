1fac2702a278d1fddbc81a70caf479b6a83ae702
Priyanshu Raghuvanshi <priyansh0975@gmail.com>
2026-03-18 22:17:08 +0530

    Time: 1 ms (99.82%), Space: 56 MB (15.23%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,5 +1,6 @@
 class Solution {
     public int majorityElement(int[] nums) {
+        // Arrays.sort(nums);
         int ele=nums[0];
         int c=0;
         for(int i=0;i<nums.length;i++){
@@ -13,7 +14,7 @@ class Solution {
             else{
                 c--;
             }
-        } 
+        }
         return ele;
     }
 }
