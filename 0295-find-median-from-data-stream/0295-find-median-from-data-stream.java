b1ce7cfd4f5318d0e7afba13ceee2515327cca3d
Priyanshu Raghuvanshi <priyansh0975@gmail.com>
2026-03-20 23:35:40 +0530

    Time: 213 ms (27.86%), Space: 111.8 MB (20.2%) - LeetHub

---
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
@@ -0,0 +1,62 @@
+// class MedianFinder {
+//     private ArrayList<Integer> list;
+//     public MedianFinder() {
+//         list=new ArrayList<>();
+//     }
+    
+//     public void addNum(int num) {
+//         list.add(num);
+//         Collections.sort(list);
+//     }
+    
+//     public double findMedian() {
+//         int n=list.size();
+//         if(n%2==0){
+//             return (list.get(n/2-1)+list.get(n/2))/2.0;
+//         }
+//        return list.get(n/2);
+//     }
+// }
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder obj = new MedianFinder();
+ * obj.addNum(num);
+ * double param_2 = obj.findMedian();
+ */
+
+
+
+
+
+
+
+
+import java.util.PriorityQueue;
+import java.util.Collections;
+
+class MedianFinder {
+    private PriorityQueue<Integer> maxHeap; // smaller half
+    private PriorityQueue<Integer> minHeap; // larger half
+
+    public MedianFinder() {
+        maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        minHeap = new PriorityQueue<>();
+    }
+
+    public void addNum(int num) {
+        maxHeap.offer(num);
+        minHeap.offer(maxHeap.poll());
+        if (maxHeap.size() < minHeap.size()) {
+            maxHeap.offer(minHeap.poll());
+        }
+    }
+
+    public double findMedian() {
+        if (maxHeap.size() > minHeap.size()) {
+            return maxHeap.peek();
+        } else {
+            return (maxHeap.peek() + minHeap.peek()) / 2.0;
+        }
+    }
+}
