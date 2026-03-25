28d42112c93bdd29483ccdbd428161312ee678b7
Priyanshu Raghuvanshi <priyansh0975@gmail.com>
2026-03-25 14:34:13 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -1,9 +1,19 @@
 class Solution {
     public boolean searchMatrix(int[][] arr, int target) {
-        for(int i=0;i<arr.length;i++){
-            for(int j=0;j<arr[0].length;j++){
-                if(arr[i][j]==target) return true;
-            }
+        // for(int i=0;i<arr.length;i++){
+        //     for(int j=0;j<arr[0].length;j++){
+        //         if(arr[i][j]==target) return true;
+        //     }
+        // }
+
+        int left=0;
+        int right=arr[0].length-1;
+        
+        while(left<arr.length && right>=0){
+            if(arr[left][right]==target) return true;
+
+            if(arr[left][right]<target) left++;
+            else right--;
         }
         return false;
     }
