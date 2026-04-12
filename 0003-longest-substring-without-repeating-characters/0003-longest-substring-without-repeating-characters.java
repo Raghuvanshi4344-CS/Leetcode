76011883c12a365531e1b7623f77d0ba619c9a18
Priyanshu Raghuvanshi <priyansh0975@gmail.com>
2026-04-12 16:14:35 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -6,8 +6,7 @@ class Solution {
         for(int i=0;i<s.length();i++){
             char c=s.charAt(i);
             if(map.containsKey(c) && map.get(c)>=x){
-               x=map.get(c)+1;
-            //    x++;
+                x=map.get(c)+1;
             }
             map.put(c,i);
             y=Math.max(y,i-x+1);
