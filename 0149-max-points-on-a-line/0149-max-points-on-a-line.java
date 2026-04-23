9e0bf64700a951efc9acb77786ace12f9b463144
Priyanshu Raghuvanshi <priyansh0975@gmail.com>
2026-04-23 14:13:41 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.java
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.java
@@ -1,6 +1,5 @@
 class Solution {
     public int maxPoints(int[][] points) {
-        if(points.length<=2) return points.length;
         int max=0;
         for(int i=0;i<points.length;i++){
             int count=0;
@@ -9,7 +8,6 @@ class Solution {
             for(int j=i+1;j<points.length;j++){
                 int dx=points[j][0]-points[i][0];
                 int dy=points[j][1]-points[i][1];
-
                 if(dx==0 && dy==0){
                     count++;
                     continue;
@@ -19,14 +17,14 @@ class Solution {
                 dy/=gcd;
                 String slope=dx+"/"+dy;
                 map.put(slope,map.getOrDefault(slope,0)+1);
-                currmax=Math.max(currmax,map.get(slope));
+                currmax=Math.max(map.get(slope),currmax);
             }
             max=Math.max(currmax+count+1,max);
         }
         return max;
     }
 
-    private int gcd(int a,int b){
+    public int gcd(int a,int b){
         if(b==0) return a;
         return gcd(b,a%b);
     }
