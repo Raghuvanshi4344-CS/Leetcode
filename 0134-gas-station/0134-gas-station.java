6394519eb46d56ca380996fcacf7eb25b4a56500
Priyanshu Raghuvanshi <priyansh0975@gmail.com>
2026-05-03 12:57:21 +0530

    Time: 2 ms (99.32%), Space: 119.5 MB (44.53%) - LeetHub

---
+++ 0134-gas-station/0134-gas-station.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int a=0;
+        int t=0;
+        int c=0;
+        for(int i=0;i<cost.length;i++){
+            int dif=gas[i]-cost[i];
+            t+=dif;
+            c+=dif;
+            if(c<0){
+                a=i+1;
+                c=0;
+            }
+        }
+        return t>=0?a:-1;
+    }
+}
