a961f51ff63409f838103a2277bcc1e0d7eaa437
Priyanshu Raghuvanshi <priyansh0975@gmail.com>
2026-05-23 01:31:16 +0530

    Time: 4 ms (94.25%), Space: 77.2 MB (67.22%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -5,17 +5,15 @@ class Solution {
         int right=arr.length-1;
         while(left<right){
             int temp=0;
-            // int temp=Math.min(arr[left],arr[right])*(right-left);
-            // max=Math.max(temp,max);
             if(arr[left]<arr[right]){
                 temp=arr[left]*(right-left);
                 left++;
             }
-            else{
+            else {
                 temp=arr[right]*(right-left);
                 right--;
             }
-            if(temp>max) max=temp;
+            max=Math.max(max,temp);
         }
         return max;
     }
