52da8161e361b9b5f860db5f80177659e90248a6
Priyanshu Raghuvanshi <priyansh0975@gmail.com>
2026-06-25 11:39:15 +0530

    Time: 0 ms (100%), Space: 48.1 MB (95.37%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
@@ -1,21 +1,29 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        HashMap<Integer,Integer> map=new HashMap<>();
-        int uq=0;
+        // HashMap<Integer,Integer> map=new HashMap<>();
+        // int uq=0;
+        // for(int i=0;i<nums.length;i++){
+        //     if(map.containsKey(nums[i])){
+        //         if (map.get(nums[i]) < 2) {
+        //             map.put(nums[i], map.get(nums[i]) + 1);
+        //             nums[uq] = nums[i];
+        //             uq++;
+        //         }
+        //     }
+        //     else{ 
+        //         map.put(nums[i],1);
+        //         nums[uq] = nums[i];
+        //         uq++;
+        //     }
+        // }
+        // return uq;
+        int k=0;
         for(int i=0;i<nums.length;i++){
-            if(map.containsKey(nums[i])){
-                if (map.get(nums[i]) < 2) {
-                    map.put(nums[i], map.get(nums[i]) + 1);
-                    nums[uq] = nums[i];
-                    uq++;
-                }
-            }
-            else{ 
-                map.put(nums[i],1);
-                nums[uq] = nums[i];
-                uq++;
+            if(k<2 || nums[i]!=nums[k-2]){
+                nums[k]=nums[i];
+                k++;
             }
         }
-        return uq;
+        return k;
     }
 }
