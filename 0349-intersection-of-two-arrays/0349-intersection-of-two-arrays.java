9e52f1050e55438cb86b03b0f6ad745c8b0b0a46
Priyanshu Raghuvanshi <priyansh0975@gmail.com>
2026-06-26 12:06:17 +0530

    Time: 2 ms (98.26%), Space: 45.3 MB (5.85%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
@@ -1,52 +1,20 @@
-// class Solution {
-//     public int[] intersection(int[] nums1, int[] nums2) {
-//         // if(nums1.length<2 || nums2.length<2) return nums1;
-//         Map<Integer,Integer> map=new HashMap<>();
-
-//         for(int i=0;i<nums1.length;i++){
-//             for(int j=0;j<nums2.length;j++){
-//                 if(nums1[i]==nums2[j]){
-//                     if(!map.containsKey(nums1[i])){
-//                         map.put(nums1[i],1);
-//                     }
-//                 }
-//             }
-//         }
-//         int[] result = new int[map.size()];
-//         int index = 0;
-//         for (int key : map.keySet()) {
-//             result[index++] = key;
-//         }
-//         return result;
-//     }
-// }
-
-
-
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-        Set<Integer> set1 = new HashSet<>();
-        Set<Integer> resultSet = new HashSet<>();
-
-        // Store nums1 elements
-        for (int num : nums1) {
-            set1.add(num);
+        Set<Integer> s1=new HashSet<>();
+        Set<Integer> res=new HashSet<>();
+        for(int i:nums1){
+            s1.add(i);
         }
 
-        // Check nums2 against set1
-        for (int num : nums2) {
-            if (set1.contains(num)) {
-                resultSet.add(num);
-            }
+        for(int i:nums2){
+            if(s1.contains(i)) res.add(i);
         }
 
-        // Convert set to array
-        int[] result = new int[resultSet.size()];
-        int i = 0;
-        for (int num : resultSet) {
-            result[i++] = num;
+        int []r=new int[res.size()];
+        int i=0;
+        for(int j:res){
+            r[i++]=j;
         }
-
-        return result;
+        return r;
     }
 }
