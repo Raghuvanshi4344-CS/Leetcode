b74bba1ad62e5131a886f971a9399e3e3fb81982
Priyanshu Raghuvanshi <priyansh0975@gmail.com>
2026-06-30 23:55:03 +0530

    Time: 3 ms (88.9%), Space: 47.3 MB (92.43%) - LeetHub

--- 0135-candy/0135-candy.java
+++ 0135-candy/0135-candy.java
@@ -3,19 +3,15 @@ class Solution {
         int min[]=new int[arr.length];
         Arrays.fill(min,1);
         for(int i=1;i<arr.length;i++){
-            if(arr[i]>arr[i-1]){
-                min[i]=min[i-1]+1;
-            }
+            if(arr[i]>arr[i-1]) min[i]=min[i-1]+1;
         }
         for(int i=arr.length-2;i>=0;i--){
-            if(arr[i]>arr[i+1]){
-                min[i]=Math.max(min[i],min[i+1]+1);
-            }
+            if(arr[i+1]<arr[i]) min[i]=Math.max(min[i],min[i+1]+1);
         }
-        int s=0;
+        int count=0;
         for(int a:min){
-            s+=a;
+            count+=a;
         }
-        return s;
+        return count;
     }
 }
