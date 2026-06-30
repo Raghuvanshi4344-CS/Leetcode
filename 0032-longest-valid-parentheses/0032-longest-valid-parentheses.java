3feef1618afd6c3783cfbad7d9c9500390d12cd0
Priyanshu Raghuvanshi <priyansh0975@gmail.com>
2026-06-30 23:24:41 +0530

    Time: 5 ms (82.45%), Space: 46.7 MB (26.38%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.java
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.java
@@ -1,46 +1,22 @@
-// class Solution {
-//     public int longestValidParentheses(String s) {
-//         Stack<Character> st=new Stack<>();
-//         int count=0;
-//         for(int i=0;i<s.length();i++){
-//             int c=s.charAt(i);
-//             if(c=='(') st.push(')');
-//             else if(!st.empty() && st.pop()==c)count+=2;
-//         }
-//         return count;
-//     }
-// }
 class Solution {
     public int longestValidParentheses(String s) {
-        int open = 0, close = 0, maxLen = 0;
+        Stack<Integer> st=new Stack<>();
+        st.push(-1);
+        int max = 0;
 
-        // Left to right pass
         for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            if (ch == '(') open++;
-            else close++;
 
-            if (open == close) {
-                maxLen = Math.max(maxLen, 2 * close);
-            } else if (close > open) {
-                open = close = 0;
+            if (s.charAt(i) == '(') {
+                st.push(i);
+            } else {
+                st.pop();
+                if (st.isEmpty()) {
+                    st.push(i);
+                } else {
+                    max = Math.max(max, i - st.peek());
+                }
             }
         }
-
-        // Right to left pass
-        open = close = 0;
-        for (int i = s.length() - 1; i >= 0; i--) {
-            char ch = s.charAt(i);
-            if (ch == '(') open++;
-            else close++;
-
-            if (open == close) {
-                maxLen = Math.max(maxLen, 2 * open);
-            } else if (open > close) {
-                open = close = 0;
-            }
-        }
-
-        return maxLen;
+        return max;
     }
 }
