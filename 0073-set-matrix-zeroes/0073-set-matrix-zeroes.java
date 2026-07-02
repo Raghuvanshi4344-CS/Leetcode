13376f8828487b49b866f130e0068f21893d44c6
Priyanshu Raghuvanshi <priyansh0975@gmail.com>
2026-07-02 23:29:03 +0530

    Time: 17 ms (5.25%), Space: 48.3 MB (5.25%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,26 +1,22 @@
-import java.util.*;
 class Solution {
-    public void helper(int [][]arr,int i,int j){
-        for(int k=0;k<arr[0].length;k++){
-            arr[i][k]=0;
-        }
-        for(int k=0;k<arr.length;k++){
-            arr[k][j]=0;
-        }
-    }
     public void setZeroes(int[][] matrix) {
-        int arr[][]=new int [matrix.length][matrix[0].length];
-        for (int i = 0; i < arr.length; i++) {
-            Arrays.fill(arr[i], -1);
-        }
+        
+        List<Pair<Integer,Integer>> re=new ArrayList<>();
         for(int i=0;i<matrix.length;i++){
             for(int j=0;j<matrix[0].length;j++){
-                if(matrix[i][j]==0) helper(arr,i,j);
+                if(matrix[i][j]==0){
+                    re.add(new Pair<>(i,j));
+                }
             }
         }
-        for(int i=0;i<matrix.length;i++){
+        for(int p=0;p<re.size();p++){
+            int row=re.get(p).getKey();
+            int col=re.get(p).getValue();
+            for(int i=0;i<matrix.length;i++){
+                matrix[i][col]=0;
+            }
             for(int j=0;j<matrix[0].length;j++){
-                if(arr[i][j]==0) matrix[i][j]=0;
+                matrix[row][j]=0;
             }
         }
     }
