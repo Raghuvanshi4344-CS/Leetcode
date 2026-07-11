a9ffa8fb3c5715ea3421001b13c647cd5aca4110
Priyanshu Raghuvanshi <priyansh0975@gmail.com>
2026-07-12 00:34:39 +0530

    Time: 6 ms (4.58%), Space: 48.3 MB (34.92%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,17 +1,17 @@
 class Solution {
-    public int trap(int[] arr) {
-        int area=0;
+    public int trap(int[] height) {
+        int max=0;
         Stack<Integer> st=new Stack<>();
-        for(int i=0;i<arr.length;i++){
-            while(!st.isEmpty() && arr[st.peek()]<arr[i]){
+        for(int i=0;i<height.length;i++){
+            while(!st.isEmpty() && height[i]>height[st.peek()]){
                 int temp=st.pop();
-                if(st.isEmpty()) break;
+                if(st.isEmpty())break;
                 int diff=i-st.peek()-1;
-                int depth=Math.min(arr[i],arr[st.peek()])-arr[temp];
-                area+=diff*depth;
+                int depth=Math.min(height[i],height[st.peek()])-height[temp];
+                max+=diff*depth;
             }
             st.push(i);
         }
-        return area;
+        return max;
     }
 }
