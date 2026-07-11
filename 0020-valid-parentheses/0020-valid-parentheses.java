e85f3be5fa90d8393aa9729f23af5759a14bc4a3
Priyanshu Raghuvanshi <priyansh0975@gmail.com>
2026-07-11 23:53:24 +0530

    Time: 3 ms (87.04%), Space: 43.1 MB (85.65%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -5,8 +5,8 @@ class Solution {
         for(int i=0;i<s.length();i++){
             char c=s.charAt(i);
             if(c=='(') st.push(')');
-            else if(c=='[') st.push(']');
             else if(c=='{') st.push('}');
+            else if(c=='[') st.push(']');
             else if(st.isEmpty() || st.pop()!=c) return false;
         }
         return st.isEmpty();
