cf33cae1200c4ce655e07f7107c32db2e010e24b
Priyanshu Raghuvanshi <priyansh0975@gmail.com>
2026-07-11 11:58:14 +0530

    Time: 3 ms (87.04%), Space: 43.3 MB (41.31%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,20 +1,14 @@
 class Solution {
     public boolean isValid(String s) {
-        // if(s.length()<2) return false;
-        // if(s.charAt(0)==')' ||s.charAt(0)==']' ||s.charAt(0)=='}') return false;
-        Stack<Character> re=new Stack<>();
+        if (s.length()<2) return false;
+        Stack<Character> st=new Stack<>();
         for(int i=0;i<s.length();i++){
-            if(s.charAt(i)=='(')re.push('(');
-            else if(s.charAt(i)=='[')re.push('[');
-            else if(s.charAt(i)=='{')re.push('{');
-            else if(s.charAt(i)==')'&& !re.isEmpty() && re.peek()=='(' ) re.pop();
-            else if(s.charAt(i)==']'&& !re.isEmpty()&& re.peek()=='[') re.pop();
-            else if(s.charAt(i)=='}' && !re.isEmpty()&& re.peek()=='{') re.pop();
-            else{
-                return false;
-            }
+            char c=s.charAt(i);
+            if(c=='(') st.push(')');
+            else if(c=='{') st.push('}');
+            else if(c=='[') st.push(']');
+            else if(st.isEmpty() || st.pop()!=c) return false;
         }
-        if(re.isEmpty()) return true;
-        else return false;
+        return st.isEmpty();
     }
 }
