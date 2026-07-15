a0c78038013b3a3c29d26f500e01ccbea0e53ca5
Priyanshu Raghuvanshi <priyansh0975@gmail.com>
2026-07-15 14:28:03 +0530

    Time: 3 ms (87.05%), Space: 43.4 MB (41.6%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,13 +1,12 @@
 class Solution {
     public boolean isValid(String s) {
-        if(s.length()<2) return false;
         Stack<Character> st=new Stack<>();
         for(int i=0;i<s.length();i++){
-            char c=s.charAt(i);
+            int c=s.charAt(i);
             if(c=='(') st.push(')');
-            else if(c=='{') st.push('}');
             else if(c=='[') st.push(']');
-            else if(st.isEmpty() || st.pop()!=c) return false;
+            else if(c=='{') st.push('}');
+            else if(st.isEmpty() || c!=st.pop()) return false;
         }
         return st.isEmpty();
     }
