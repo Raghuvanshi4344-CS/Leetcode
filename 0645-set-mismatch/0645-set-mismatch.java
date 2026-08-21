93459041fccd162aae4f1b8d9ae822f155be6e17
Priyanshu Raghuvanshi <priyansh0975@gmail.com>
2026-08-21 11:54:50 +0530

    Time: 14 ms (31.87%), Space: 47.3 MB (94.08%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -1,22 +1,23 @@
 class Solution {
-    public int[] findErrorNums(int[] arr) {
-        int i=0;
-        for(;i<arr.length;){
-            int ci=arr[i]-1;
-            if(arr[i]>=1 && arr[i]<=arr.length  && arr[i]!=arr[ci]){
-                int temp=arr[i];
-                arr[i]=arr[ci];
-                arr[ci]=temp;
-            }
-            else{
-                i++;
-            }
+    public int[] findErrorNums(int[] nums) {
+        int s=0,ss=0;
+        for(int i=1;i<=nums.length;i++){
+            s+=i;
+            ss+=nums[i-1];
         }
-        for(i=0;i<arr.length;i++){
-            if(arr[i]!=i+1){
-                return new int[]{arr[i],i+1};
+        HashMap<Integer,Integer> hm=new HashMap<>();
+        System.out.println(ss);
+        System.out.println(s);
+        int arr[]=new int[2];
+        for(int i=0;i<nums.length;i++){
+            if(hm.containsKey(nums[i])){
+                arr[0]=nums[i];
+                ss-=nums[i];
+                arr[1]=s-ss;
+                return arr;
             }
+            hm.put(nums[i],i);
         }
-        return new int[]{-1,-1};
+        return arr;
     }
 }
