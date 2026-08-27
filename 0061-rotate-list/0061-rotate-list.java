716ccd9ac9ab9ec1ce529f5d1db3bcbd63475fe7
Priyanshu Raghuvanshi <priyansh0975@gmail.com>
2026-08-27 12:47:35 +0530

    Time: 0 ms (100%), Space: 44.3 MB (44.42%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.java
+++ 0061-rotate-list/0061-rotate-list.java
@@ -11,7 +11,7 @@
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
         if(head==null||head.next==null)return head;
-        ListNode tem=head;
+        ListNode tem=head,f=head,s=head;
         int c=0;
         while(tem!=null){
             c++;
@@ -19,16 +19,16 @@ class Solution {
         }
         k=k%c;
         while(k>0){
-            ListNode tr=head;
-            while(tr!=null && tr.next.next!=null){
-                tr=tr.next;
-            }
-            ListNode temp=tr.next;
-            tr.next=null;
-            temp.next=head;
-            head=temp;
+            f=f.next;
             k--;
         }
+        while(f.next!=null){
+            f=f.next;
+            s=s.next;
+        }
+        f.next=head;
+        head=s.next;
+        s.next=null;
         return head;
     }
 }
