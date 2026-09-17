d3979c54835b05f656eeb1e1c8b193b97e38712d
Priyanshu Raghuvanshi <priyansh0975@gmail.com>
2026-09-17 12:37:21 +0530

    Time: 2 ms (69.42%), Space: 44.6 MB (64.25%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,12 +1,12 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
-        List<String> s=new LinkedList<>();
-        func(s,"",n,0,0);
-        return s;
+        List<String> re=new ArrayList<>();
+        func(re,"",n,0,0);
+        return re;
     }
-    public static void func(List<String> s,String temp,int n,int o,int c){
-        if(o==n && o==c) s.add(temp);
-        if(o<n) func(s,temp+"(",n,o+1,c);
-        if(c<o) func(s,temp+")",n,o,c+1);
+    void func(List<String> s,String temp,int n,int o,int c){
+        if(o==n&&c==o) s.add(temp);
+        if(o<n)func(s,temp+"(",n,o+1,c);
+        if(c<o)func(s,temp+")",n,o,c+1);
     }
 }
