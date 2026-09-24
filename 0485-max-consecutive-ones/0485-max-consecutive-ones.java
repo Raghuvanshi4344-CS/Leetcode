9d236589dca34a9a85d53e5d4a5511007119b2ca
Priyanshu Raghuvanshi <priyansh0975@gmail.com>
2026-09-24 13:25:23 +0530

    Time: 2 ms (98.26%), Space: 52.4 MB (83.64%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.java
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.java
@@ -3,12 +3,13 @@ class Solution {
         int c=0;
         int temp=0;
         for(int i:nums){
-            if(i==1)temp++;
-            else{
+            if(i==1) temp++;
+            else if(i==0){
+                c=Math.max(temp,c);
                 temp=0;
             }
-            c=Math.max(c,temp);
         }
+        c=Math.max(c,temp);
         return c;
     }
 }
